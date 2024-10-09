f6573ef8e0f3f3588c33beb2e4b7beea93ecb097
hanna <hannaeinarson93@hotmail.com>
2024-10-09 16:01:28 +0200

    adding

--- main.java
+++ main.java
@@ -1,1 +1,17 @@
-System.out.println("Hej");
+public class main {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+        numbers.add(1);
+        numbers.add(3);
+        numbers.add(6);
+        numbers.add(8);
+        numbers.add(10);
+
+        for(int i = 0; i < numbers.size(); i++ ) {
+            int number = numbers.get(i);
+            System.out.println(number);
+        }
+    }
+}
